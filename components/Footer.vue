e6d16b48265009783a25fcb60f6cbea2e4089198
dananj <djaya161@gmail.com>
2021-02-21 16:32:24 +0800

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+<h1>footer</h1>
+</div>
+</template>
+
+<style>
+
+</style>
+<script lang="ts">
+
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <Header />
+        <Nuxt />
+      <Footer/>
+    </div>
+</template>
+
+<style>
+
+</style>
+<script lang="ts">
+import Header from "~/components/Header.vue"
+import Footer from "~/components/Footer.vue"
+export default {
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
